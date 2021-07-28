058e05dd9fc9284bb8a254e52b9cb0298abd8eab
KJ Sudarshan <kjsudi@gmail.com>
2021-07-28 20:17:02 +0530

    add: react training sessions 1-3

---
+++ gatsby-config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  plugins: [
+    `gatsby-plugin-sharp`,
+    `gatsby-remark-images`,
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1200,
+            },
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/pages`,
+      },
+    },
+  ],
+};
